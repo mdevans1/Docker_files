9dab55f5a0f780db6f26883d91ae3ff17c24b4d3
mdevans1 <mdevans@udel.edu>
2014-10-31 10:48:18 -0400

    added docker checks

---
+++ sensu/client_script/install_dockerchecks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -O /etc/sensu/plugins/check-container.rb https://raw.github.com/sensu/sensu-community-plugins/master/plugins/docker/check-container.rb
+wget -O /etc/sensu/plugins/check-docker-container.rb https://raw.github.com/sensu/sensu-community-plugins/master/plugins/docker/check-docker-container.rb
+wget -O /etc/sensu/plugins/docker-container-metrics.rb https://raw.github.com/sensu/sensu-community-plugins/master/plugins/docker/docker-container-metrics.rb
+chmod 755 /etc/sensu/plugins/*.rb
